87471b162155f70ae62e16052f48aea0b9774a6b
Sakuric <qingmengzhong0.@gmail.com>
2026-02-22 01:32:35 +0800

    仓库初始化

---
+++ src/main/java/com/example/oilrisk_alert/OilRiskAlertApplication.java
@@ -0,0 +1,13 @@
+package com.example.oilrisk_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilRiskAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilRiskAlertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oilrisk_alert/OilRiskAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oilrisk_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilRiskAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
